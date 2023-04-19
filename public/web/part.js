9429c61cabb8f09de0d583d2aadda03b2d9e8b30
pol6pil <93617401+pol6pil@users.noreply.github.com>
2023-04-19 23:55:29 +0200

    d

---
+++ public/web/part.js
@@ -0,0 +1,9 @@
+'use strict'
+
+// Obtenemos el id de la pieza deseada
+const urlParams = new URLSearchParams(window.location.search)
+const partId = urlParams.get('id') || 0
+
+async function mostrarPieza(){
+
+}
