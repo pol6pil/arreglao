22cded0aaab30d2d6a4800a2da5ba0ea1b81cf01
pol6pil <93617401+pol6pil@users.noreply.github.com>
2023-04-24 23:29:02 +0200

    mostrar detalle pieza (sin carrito o reviews)

---
+++ public/web/scripts/header.js
@@ -0,0 +1,59 @@
+'use strict'
+
+// Mostramos el header de las paginas
+
+function mostrarHeader () {
+  const header = document.createElement('header')
+  const aboutUs = document.createElement('a')
+  aboutUs.setAttribute('href', './aboutUs.html')
+  aboutUs.append('Sobre nosotros')
+  header.append(aboutUs)
+  const store = document.createElement('a')
+  store.setAttribute('href', './index.html')
+  store.append('Tienda')
+  header.append(store)
+  const guides = document.createElement('a')
+  guides.setAttribute('href', './guides.html')
+  header.append(guides)
+  guides.append('Guias')
+
+  const searchForm = document.createElement('form')
+  searchForm.name = 'search'
+  const searchInput = document.createElement('input')
+  searchInput.type = 'text'
+  searchInput.placeholder = 'Buscar'
+  const searchButton = document.createElement('button')
+  searchButton.type = 'submit'
+  const searchIcon = document.createElement('img')
+  searchIcon.setAttribute('src', './images/icons/search.png')
+  searchButton.append(searchIcon)
+  searchForm.append(searchInput)
+  searchForm.append(searchButton)
+  header.append(searchForm)
+
+  // Si el usuario no esta logeado
+  if (window.localStorage.getItem('user') === null) {
+    const login = document.createElement('a')
+    login.setAttribute('href', './login.html')
+    login.append('Iniciar Sesion')
+    header.append(login)
+    const register = document.createElement('a')
+    register.setAttribute('href', './register.html')
+    register.append('Registrarse')
+    header.append(register)
+  } else {
+    // Si el usuario esta logeado
+    const logoff = document.createElement('a')
+    logoff.setAttribute('href', './index.html')
+    logoff.onclick = () => { window.localStorage.removeItem('user') }
+    logoff.append('Cerrar Sesion')
+    header.append(logoff)
+
+    const pfpA = document.createElement('a')
+  }
+
+  const pfpA = document.createElement('a')
+
+  document.body.prepend(header)
+}
+mostrarHeader()
--- public/web/scripts/part.js
+++ public/web/scripts/part.js
@@ -2,12 +2,122 @@
 async function mostrarPieza (id) {
   const part = await obtenerPieza(id)
   console.log(part)
+
+  // Mostramos la pieza
+  const productOverview = document.querySelector('#productOverview')
+
+  const divImage = document.createElement('div')
+  divImage.className = 'productImage'
+  const image = document.createElement('img')
+  divImage.append(image)
+
+  const price = document.querySelector('#productInfoPrice')
+  mostrarOpcion(part.options[0], image, price)
+
+  const divProductInfo = document.querySelector('#divProductInfo')
+  const h1 = document.querySelector('#divProductInfo h1')
+  h1.append(part.name)
+
+  const description = document.querySelector('#productDetail p')
+  description.append(part.description)
+
+  const partWarranty = document.querySelector('#partWarranty')
+
+  if (part.warranty > 1) {
+    partWarranty.append(`Garantia de ${part.warranty} años`)
+  } else if (part.warranty === 1) {
+    partWarranty.append('Garantia de 1 año')
+  } else if (part.warranty === 0) {
+    partWarranty.append('Garantia de por vida')
+  }
+
+  // Obtenemos el electrodomestico para mostrar el nombre
+  const appliance = await obtenerElectrodomestico(part.appliance)
+  const partAppliance = document.querySelector('#partAppliance')
+  partAppliance.append(appliance.name)
+  partAppliance.setAttribute('href', `./parts.html?appliance=${part.appliance}`)
+  // const lastDiv = document.querySelector('#divProductInfo div:last-child')
+
+  // Si el producto tiene nota se muestra
+  if (part.note.length > 0) {
+    const div = document.querySelector('#productDetail div:first-child')
+    showInfoBox('note', part.note, div)
+  }
+  // Igual con la advertencia
+  if (part.warning.length > 0) {
+    const div = document.querySelector('#productDetail div:first-child')
+    showInfoBox('warning', part.warning, div)
+  }
+
+  // Si la pieza tiene mas de una opcion las mostramos en un select
+  if (part.options.length > 1) {
+    const select = document.createElement('select')
+    select.id = 'optionSelect'
+
+    // Guardamos el indice de las opciones para ponerselo como value
+    let optionsQuantity = 0
+    for (const option of part.options) {
+      const selectOption = document.createElement('option')
+      selectOption.value = optionsQuantity
+      selectOption.append(option.name)
+      select.append(selectOption)
+
+      select.onchange = (e) => {
+        mostrarOpcion(part.options[e.target.value], image, price)
+      }
+      optionsQuantity++
+    }
+    divProductInfo.insertBefore(select, divProductInfo.children[3])
+  } else {
+    const option = document.createElement('span')
+    option.append(`Option: ${part.options[0].name}`)
+    divProductInfo.insertBefore(option, divProductInfo.children[3])
+  }
+
+  productOverview.prepend(divImage)
+}
+
+function mostrarOpcion (option, img, span) {
+  img.setAttribute('src', option.imgUrl)
+  span.innerText = `${option.price}€`
 }
 
 async function obtenerPieza (id) {
   const res = await fetch(`http://localhost/parts/${id}`)
   return await res.json()
 }
+
+async function obtenerElectrodomestico (id) {
+  const res = await fetch(`http://localhost/appliances/${id}`)
+  return await res.json()
+}
+
+function showInfoBox (type, content, div) {
+  const infoDiv = document.createElement('div')
+  const infoSpan = document.createElement('span')
+  if (type === 'note') {
+    infoDiv.className = 'note'
+    infoSpan.append('NOTA')
+  } else if (type === 'warning') {
+    infoDiv.className = 'warning'
+    infoSpan.append('ADVERTENCIA')
+  } else {
+    return
+  }
+
+  const infoContent = document.createElement('p')
+  infoContent.append(content)
+  infoDiv.append(infoSpan)
+  infoDiv.append(infoContent)
+
+  div.append(infoDiv)
+}
+// async function mostrarReviews () {
+
+// }
+
+// async function obtenerReviews (id) {
+// }
 
 // Obtenemos el id de la pieza deseada
 const urlParams = new URLSearchParams(window.location.search)
