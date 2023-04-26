9fbb7d76c0b657714ea59560f6a08e107c6fd9f2
pol6pil <93617401+pol6pil@users.noreply.github.com>
2023-04-26 23:53:13 +0200

    validacion formulario contraseña

--- public/web/part.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-// Obtenemos el id de la pieza deseada
-const urlParams = new URLSearchParams(window.location.search)
-const partId = urlParams.get('id') || 0
-
-async function mostrarPieza(){
-
-}
--- public/web/scripts/register.js
+++ public/web/scripts/register.js
@@ -7,11 +7,48 @@ async function insertPart (formData) {
   console.log(json)
 }
 
+function validatePassword () {
+  const password = document.forms.register.password.value
+  if (password.length < 6) {
+    const span = document.querySelector('#validatingPasswordError')
+    span.className = 'error'
+    return false
+  } else {
+    const span = document.querySelector('#validatingPasswordError')
+    span.className = 'hidden'
+  }
+  const password2 = document.forms.register.password2.value
+  // eslint-disable-next-line eqeqeq
+  if (password != password2) {
+    const span = document.querySelector('#matchingPasswordsError')
+    span.className = 'error'
+    return false
+  } else {
+    const span = document.querySelector('#matchingPasswordsError')
+    span.className = 'hidden'
+  }
+  return true
+}
 const form = document.forms.register
+
+form.password.onchange = (e) => {
+  if (validatePassword()) {
+    form.submit.disabled = false
+  } else {
+    form.submit.disabled = true
+  }
+}
+form.password2.onchange = (e) => {
+  if (validatePassword()) {
+    form.submit.disabled = false
+  } else {
+    form.submit.disabled = true
+  }
+}
 
 form.onsubmit = (e) => {
   e.preventDefault()
-  
+
   const formData = new FormData(form)
   insertPart(formData)
 }
